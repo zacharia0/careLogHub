227b6aa61a774a1a31c5f47a687e9937f4217798
Zacharia Dolal <zacharia.dolal@gmail.com>
2024-09-04 14:46:45 -0500

    Connected to the database.

--- careLogHub-be/server.js
+++ careLogHub-be/server.js
@@ -1,1 +1,19 @@
 const express = require("express")
+const app = express()
+const mongoose = require("mongoose")
+require('dotenv').config()
+
+//middleware
+app.use(express.json())
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log("connected to database")
+        app.listen(process.env.PORT, () =>{
+            console.log("Listening for request on port" , process.env.PORT)
+        })
+
+    })
+.catch((err) =>{
+    console.log(err)
+})
