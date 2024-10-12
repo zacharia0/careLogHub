b36144b40e1b0f56c65d9e73860a8eb5c94c1915
Zacharia Dolal <zacharia.dolal@gmail.com>
2024-10-12 18:39:11 -0500

    feat: create Client model with schema
    - Defined clientSchema for MongoDB with fields: firstName, middleName, lastName, dateOfBirth, moveInDate, roomNumber, foodAllergy, medicalAllergy, emergencyContact, diagnoses, primaryMedicalContact, and guardian.
    - Set validation rules for required fields.
    - Established default values for roomNumber, foodAllergy, and medicalAllergy.
    - Exported the Client model for use in the application.

--- careLogHub-be/models/clientModel.js
+++ careLogHub-be/models/clientModel.js
@@ -1,18 +1,53 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose")
 
 const clientSchema = mongoose.Schema({
-
-        firstName: {
-            type: String,
-            required: true
-        },
-        lastName: {
-            type: String,
-            required: true
-        },
-
+    firstName:{
+        type:String,
+        required:true
     },
-    {timestamps:true}
-)
+    middleName:{
+        type:String,
+        required:true
+    },
+    lastName:{
+        type:String,
+        required:true
+    },
+    dateOfBirth:{
+        type:Date,
+        required:true
+    },
+    moveInDate:{
+        type:Date,
+        required:true
+    },
+    roomNumber:{
+        type:String,
+        default:"N/A"
+    },
+    foodAllergy:{
+        type:String,
+        default:"NONE"
+    },
+    medicalAllergy:{
+        type:String,
+        default:"NONE"
+    },
+    emergencyContact:{
+        type:String,
+    },
+    diagnoses:{
+        type:String,
+        required:true
+    },
+    primaryMedicalContact:{
+        type:String
+    },
+    guardian:{
+        type:String
+    },
 
-module.exports = mongoose.model("Client", clientSchema)
+
+})
+
+exports.module = mongoose.model("Client",clientSchema)
