400bd23f6d0f5d2e16c13d7cc3edf451ebb1577c
Zacharia Dolal <zacharia.dolal@gmail.com>
2024-10-28 10:59:33 -0500

    feat: Add responsive Navbar component with routing links
    - Added a responsive Navbar component using TailwindCSS and Headless UI.
    - Included routing links to main pages (Dashboard, Daily Logs, Clients, and Pass Medication) using  from  for SPA behavior.
    - Implemented mobile-friendly toggle with  for compact view on smaller screens. Used Headless UI components for profile dropdowns and responsive navigation elements.

--- careLogHub-fe/src/App.jsx
+++ careLogHub-fe/src/App.jsx
@@ -3,11 +3,13 @@ import DailyLogList from "./pages/DailyLogList.jsx";
 import DailyLogForm from "./components/DailyLogForm.jsx";
 import ClientForm from "./components/ClientForm.jsx";
 import ClientList from "./pages/ClientList.jsx";
+import Navbar from "./pages/Navbar.jsx";
 
 const App = () =>{
 
     return (
         <BrowserRouter>
+            <Navbar/>
             <Routes>
                 <Route path="/" element={<DailyLogList/>}/>
                 <Route path ="/all-daily-logs" element={<DailyLogList/>}></Route>
