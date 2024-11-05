19e3b6efc59b03586f725466db2332fe8281caf1
Zacharia Dolal <zacharia.dolal@gmail.com>
2024-11-05 09:06:06 -0600

    feat: fetch and display employee data from API on the front end
    - Created  component to fetch all employees from the API and manage the employee list state.
    - Implemented  function using  to fetch data on component mount.
    - Used  with  action to update context with fetched employee data.
    - Displayed each employee's details in the  component.
    - Added error handling and display for failed fetch attempts.
    - Connected  to render individual employee properties like first name, last name, middle name, and username.

---
+++ careLogHub-fe/src/pages/EmployeeList.jsx
@@ -0,0 +1,51 @@
+import {useEffect, useState} from "react";
+import {useEmployeeContext} from "../hooks/useEmployeeContext.js";
+import EmployeeDetail from "../components/EmployeeDetail.jsx";
+
+
+const EmployeeList = () =>{
+    const {employees,dispatch} = useEmployeeContext()
+    const [error,setError] = useState("")
+
+    useEffect(() =>{
+        const getAllEmployees = async() =>{
+            setError("")
+            const response = await fetch("http://localhost:4000/api/employee/all-employees",{
+                method:"GET"
+            })
+            const json = await response.json()
+            if(response.ok){
+                setError("")
+                dispatch({type:"SET_EMPLOYEE",payload:json})
+            }
+            if(!response.ok){
+                console.log("Failed to fetch employees")
+                setError("failed to fetch employees")
+            }
+
+        }
+
+
+        getAllEmployees()
+
+
+    })
+
+    return(
+        <div>
+            <div>
+                {error && <div>{error}</div>}
+            </div>
+            <div>
+                {employees && employees.map((employee) =>{
+                    <EmployeeDetail key = {employee._id} employees={employee}/>
+                })}
+            </div>
+
+        </div>
+
+
+    )
+}
+
+export default EmployeeList
