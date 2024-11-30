3253a0078a033c6c99a212f03f08000b907d3aa2
Zacharia Dolal <zacharia.dolal@gmail.com>
2024-11-30 11:09:47 -0600

    feat: Display appropriate error message when a client has no medications. - updated the fetchMedicationById function to handle cases wehre a client has not medications. - dispatches an empty array to clear stale data when no medications are found. - sets a clear error message to be diaplayed in the UI. - ensure that the error state is properly reset when switching between clients. - improved overall robustness of the medicaiton fetch logic.

--- careLogHub-fe/src/pages/PassMedsList.jsx
+++ careLogHub-fe/src/pages/PassMedsList.jsx
@@ -1,13 +1,16 @@
 import {useMedicationContext} from "../hooks/useMedicationContext.js";
 import {useEffect, useState} from "react";
 import {useParams} from "react-router-dom";
+import {useClientContext} from "../hooks/useClientContext.js";
 
 const PassMedsList = () => {
-    const { medicationByClient, dispatch } = useMedicationContext();
+    const {medicationByClient, dispatch} = useMedicationContext();
     const [loading, setLoading] = useState(true);
     const [activeTab, setActiveTab] = useState("morning");
     const [administeredMeds, setAdministeredMeds] = useState({});
-    const { clientId } = useParams();
+    const {clientId} = useParams();
+    const {clients} = useClientContext()
+    const [err, setError] = useState("")
 
     useEffect(() => {
         // Determine active tab based on current time
@@ -21,13 +24,32 @@ const PassMedsList = () => {
         setActiveTab(determineActiveTab());
 
         const fetchMedicationById = async () => {
-            const response = await fetch(`http://localhost:4000/api/med/${clientId}`);
-            const json = await response.json();
-            if (response.ok) {
-                dispatch({ type: "SET_MEDICATION_BY_CLIENT_ID", payload: json });
-            } else {
-                console.log("Failed to fetch Medication by Id");
+            setLoading(true)
+            try{
+                const response = await fetch(`http://localhost:4000/api/med/${clientId}`);
+                const json = await response.json();
+                console.log("FETCHING MEDICAITON BY CLIENT ID: ", json)
+                if (response.ok) {
+                    dispatch({type: "SET_MEDICATION_BY_CLIENT_ID", payload: json});
+                    setError("")
+                }else{
+                    dispatch({type: "SET_MEDICATION_BY_CLIENT_ID", payload: []});
+                    setError(json.error)
+                }
+
+            }catch(error){
+                console.log("An error occurred:",error)
+                setError("Failed to fetch medication. Please try again later.")
+                dispatch({type: "SET_MEDICATION_BY_CLIENT_ID", payload: []});
+
+            }finally {
+                setLoading(false)
             }
+
+
+
+            console.log("Failed to fetch Medication by Id");
+
             setLoading(false);
         };
         fetchMedicationById();
@@ -50,12 +72,17 @@ const PassMedsList = () => {
     if (loading) {
         return <p>Loading Medications...</p>;
     }
-    if (!medicationByClient || medicationByClient.length === 0) {
-        return <p>No Medications to pass.</p>;
-    }
 
-    const clientName = `${medicationByClient[0]?.client.firstName} ${medicationByClient[0].client.lastName}`;
+    const findClient = clients.find((client) => client._id === clientId)
+    console.log(findClient)
     const filteredMeds = medicationByClient.filter((med) => med.timeSlot.includes(activeTab));
+
+
+
+
+        const firstName = findClient?.firstName
+        const lastName = findClient?.lastName
+
 
     return (
         <div>
@@ -80,41 +107,46 @@ const PassMedsList = () => {
                 </ul>
 
                 {/* Medications List */}
-                <div className="p-6 bg-gray-50 text-medium text-gray-500 dark:text-gray-400 dark:bg-gray-800 rounded-lg w-full">
+                <div
+                    className="p-6 bg-gray-50 text-medium text-gray-500 dark:text-gray-400 dark:bg-gray-800 rounded-lg w-full">
                     <h3 className="text-lg font-bold text-gray-900 dark:text-white mb-2">
-                        Medications for: {clientName}
+                        Medications for: {firstName + " " + lastName}
                     </h3>
-                    {filteredMeds.length > 0 ? (
+                    {
+                        filteredMeds && filteredMeds.length > 0  ?
                         filteredMeds.map((med) => (
-                            <div key={med._id} className="mb-4">
-                                <div>
-                                    <label>Medication Name: </label>
-                                    {med.medName}
+                                <div key={med._id} className="mb-4">
+                                    <div>
+                                        <label>Medication Name: </label>
+                                        {med.medName}
+                                    </div>
+                                    <div className="flex space-x-2 mt-2">
+                                        {["Pass", "Refuse", "Adverse Reaction", "Other", "Administer"].map((action) => (
+                                            <button
+                                                key={action}
+                                                onClick={() => handleAction(med._id, action)}
+                                                className={`px-4 py-2 rounded ${
+                                                    action === "Administer" && administeredMeds[med._id] === activeTab
+                                                        ? "bg-gray-300 text-gray-500 cursor-not-allowed"
+                                                        : "bg-blue-500 text-white hover:bg-blue-600"
+                                                }`}
+                                                disabled={
+                                                    action === "Administer" &&
+                                                    administeredMeds[med._id] === activeTab
+                                                }
+                                            >
+                                                {action}
+                                            </button>
+                                        ))}
+                                    </div>
                                 </div>
-                                <div className="flex space-x-2 mt-2">
-                                    {["Pass", "Refuse", "Adverse Reaction", "Other", "Administer"].map((action) => (
-                                        <button
-                                            key={action}
-                                            onClick={() => handleAction(med._id, action)}
-                                            className={`px-4 py-2 rounded ${
-                                                action === "Administer" && administeredMeds[med._id] === activeTab
-                                                    ? "bg-gray-300 text-gray-500 cursor-not-allowed"
-                                                    : "bg-blue-500 text-white hover:bg-blue-600"
-                                            }`}
-                                            disabled={
-                                                action === "Administer" &&
-                                                administeredMeds[med._id] === activeTab
-                                            }
-                                        >
-                                            {action}
-                                        </button>
-                                    ))}
-                                </div>
+                            )
+                        ):(
+                            <div>
+                                {err  || "No medications found for this client."}
                             </div>
-                        ))
-                    ) : (
-                        <p>No Medications to pass.</p>
-                    )}
+                            )
+                    }
                 </div>
             </div>
         </div>
